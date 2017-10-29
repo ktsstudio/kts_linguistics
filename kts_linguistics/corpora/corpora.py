2d31effe147af410e71599cee05a91bf90054123
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-10-29 22:45:47 +0300

    Add more update functions to Corpora class

--- kts_linguistics/corpora/corpora.py
+++ kts_linguistics/corpora/corpora.py
@@ -1,6 +1,8 @@
 from collections import Counter
 from typing import List
 
+from nltk import ToktokTokenizer
+
 
 class Corpora:
     def __init__(self):
@@ -18,6 +20,22 @@ class Corpora:
 
     def update_with_counter(self, counter: Counter):
         self._counter.update(counter)
+
+    def update_with_str(self, s: str):
+        tokenizer = ToktokTokenizer()
+        self.update_with_list(tokenizer.tokenize(s))
+
+    def update_with_list_of_str(self, l: List[str]):
+        for s in l:
+            self.update_with_str(s)
+
+    def update_with_list_of_list_of_str(self, ll: List[List[str]]):
+        for l in ll:
+            for s in l:
+                self.update_with_str(s)
+
+    def update_with_corpora(self, corpora):
+        self.update_with_counter(corpora._counter)
 
     def words(self) -> List[str]:
         return list(self._counter.keys())
