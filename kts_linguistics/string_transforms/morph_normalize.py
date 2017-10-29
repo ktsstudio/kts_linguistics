1fde536df279f9e3475d4eb7cf39892ca9a7d6ce
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-10-29 22:25:29 +0300

    Add caching to MorphNormalizeTransform

--- kts_linguistics/string_transforms/morph_normalize.py
+++ kts_linguistics/string_transforms/morph_normalize.py
@@ -4,8 +4,18 @@ from kts_linguistics.string_transforms.abstract_transform import AbstractByWordT
 
 
 class MorphNormalizeTransform(AbstractByWordTransform):
-    def __init__(self):
+    def __init__(self, do_cache=False):
         self.morph = pymorphy2.MorphAnalyzer()
+        self.cache = dict()
+        self.do_cache = do_cache
 
-    def transform_word(self, s: str) -> str:
-        return self.morph.parse(s)[0].normal_form
+    def transform_word(self, word: str) -> str:
+        if word in self.cache:
+            return self.cache[word]
+
+        normalized_word = self.morph.parse(word)[0].normal_form
+
+        if self.do_cache:
+            self.cache[word] = normalized_word
+
+        return normalized_word
