e8c8f5d9d1781d94a0a64b2ddfb651dd5074b9bf
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-10-29 21:59:31 +0300

    Add BasicNormalizeByWordTransform class

--- kts_linguistics/string_transforms/basic_normalize.py
+++ kts_linguistics/string_transforms/basic_normalize.py
@@ -1,5 +1,5 @@
 from kts_linguistics.chars import RUSSIAN_ALPHABET, ENGLISH_ALPHABET, DIGITS
-from kts_linguistics.string_transforms.abstract_transform import AbstractTransform
+from kts_linguistics.string_transforms.abstract_transform import AbstractTransform, AbstractByWordTransform
 
 
 class BasicNormalizeTransform(AbstractTransform):
@@ -15,6 +15,25 @@ class BasicNormalizeTransform(AbstractTransform):
             self.allowed_chars += ' '
 
     def transform(self, s: str) -> str:
+        s = s.strip()
+        s = s.lower()
+        s = ''.join(c for c in s if c in self.allowed_chars)
+        return s
+
+
+class BasicNormalizeByWordTransform(AbstractByWordTransform):
+    def __init__(self, leave_russian_chars=True, leave_english_chars=True, leave_digits=True, leave_space=True):
+        self.allowed_chars = ''
+        if leave_russian_chars:
+            self.allowed_chars += RUSSIAN_ALPHABET
+        if leave_english_chars:
+            self.allowed_chars += ENGLISH_ALPHABET
+        if leave_digits:
+            self.allowed_chars += DIGITS
+        if leave_space:
+            self.allowed_chars += ' '
+
+    def transform_word(self, s: str) -> str:
         s = s.strip()
         s = s.lower()
         s = ''.join(c for c in s if c in self.allowed_chars)
