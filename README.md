88a1dac5db1aba7b834504f48afaada3ef45b196
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-11-05 19:06:52 +0300

    0.1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# kts_linguistics
+
+This library includes several sub-packages:
+- `spellcheck` – Fixing spelling typos
+- `phonetics` – [Russian Metaphone](http://forum.aeroion.ru/topic461.html) implementation
+- `string_transforms` – Reusable pipeline for string transformations
+- `corpora` – Loading corpora
+- `synonyms` – Finding synonyms
+- `test` – Test utilities
--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 from setuptools import setup
 
 
-dependencies = []
-with open('requirements.txt') as f:
-    for line in f:
-        if line != '':
-            dependencies.append(line)
-
 setup(
     name='kts_linguistics',
-    version='0.0.25',
+    version='0.1.0',
+    description='Set of packages: spellcheck, phonetics, text processing and more',
+    long_description=open('README.md').read(),
+
+    author='KTS',
+    author_email='o.morozenkov@ktsstudio.ru',
+    url='https://github.com/KTSStudio/kts_linguistics',
+    license='MIT',
+
+    # See: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic'
+    ],
+
+    install_requires=[
+        'transliterate>=1.10,<1.11',
+        'python-Levenshtein>=0.12,<0.13',
+        'pymorphy2>=0.8,<0.9',
+        'nltk>=3.2,<3.3',
+        'scikit-learn>=0.19,<0.20'
+    ],
+
     packages=['kts_linguistics',
               'kts_linguistics.corpora',
               'kts_linguistics.phonetics',
@@ -17,6 +34,5 @@ setup(
               'kts_linguistics.string_transforms',
               'kts_linguistics.synonyms',
               'kts_linguistics.test'],
-    install_requires=dependencies,
-    include_package_data=True
+    include_package_data=True,
 )
