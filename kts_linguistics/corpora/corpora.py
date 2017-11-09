d9ef9bf7013e8cde039ab65b72049e62f31f143c
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-11-09 18:08:02 +0300

    Prettify Corpora class

--- kts_linguistics/corpora/corpora.py
+++ kts_linguistics/corpora/corpora.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import List
+from typing import Iterable
 
 from nltk import ToktokTokenizer
 
@@ -14,34 +14,34 @@ class Corpora:
     def increment_popularity(self, word: str, count: int = 1):
         self._counter[word] += count
 
-    def update_with_list(self, words: List[str]):
+    def update_with_list(self, words: Iterable[str]):
         for word in words:
             self._counter[word] += 1
+
+    def update_with_sentence(self, s: str):
+        tokenizer = ToktokTokenizer()
+        self.update_with_list(tokenizer.tokenize(s))
+
+    def update_with_list_of_sentences(self, l: Iterable[str]):
+        for s in l:
+            self.update_with_sentence(s)
+
+    def update_with_list_of_list_of_sentences(self, ll: Iterable[Iterable[str]]):
+        for l in ll:
+            for s in l:
+                self.update_with_sentence(s)
 
     def update_with_counter(self, counter: Counter):
         self._counter.update(counter)
 
-    def update_with_str(self, s: str):
-        tokenizer = ToktokTokenizer()
-        self.update_with_list(tokenizer.tokenize(s))
-
-    def update_with_list_of_str(self, l: List[str]):
-        for s in l:
-            self.update_with_str(s)
-
-    def update_with_list_of_list_of_str(self, ll: List[List[str]]):
-        for l in ll:
-            for s in l:
-                self.update_with_str(s)
-
     def update_with_corpora(self, corpora):
         self.update_with_counter(corpora._counter)
 
-    def words(self) -> List[str]:
-        return list(self._counter.keys())
+    def words(self) -> Iterable[str]:
+        return self._counter.keys()
 
-    def words_with_counts(self) -> List[str]:
-        return list(self._counter.items())
+    def words_with_counts(self) -> Iterable[str]:
+        return self._counter.items()
 
     def __contains__(self, item: str) -> bool:
         return item in self._counter.keys()
